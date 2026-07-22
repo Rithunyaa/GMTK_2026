f09076ed41d0db4ff83d90585bf58bf78ab9479c
Rithunyaa <rithunyaa@gmail.com>
2026-07-22 16:27:51 -0400

    Player can move
    WASD also works, has collision shape

---
+++ gmtk-2026/battle_prototype/battle_scripts/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+# Player movement speed
+@export var speed: float = 200.0
+
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+
+	# WASD movement
+	if Input.is_action_pressed("walk_right"):
+		direction.x += 10
+		print("right")
+
+	if Input.is_action_pressed("walk_left"):
+		direction.x -= 10
+		print("left")
+
+	if Input.is_action_pressed("walk_down"):
+		direction.y += 10
+		print("up")
+
+	if Input.is_action_pressed("walk_up"):
+		direction.y -= 10
+		print("down")
+
+	# Prevent faster diagonal movement
+	if direction.length() > 0:
+		direction = direction.normalized()
+
+	# Apply movement
+	velocity = direction * speed
+	print ("velocity")
+
+	move_and_slide()
