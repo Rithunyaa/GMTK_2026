9acff9fa384d10c472b622f28faaae01101094e3
Mr-Excoem <chuikeeper@outlook.com>
2026-07-23 21:31:01 +0800

    improved the clean up room level's look

--- gmtk-2026/party_prototype/party_script/clothing.gd
+++ gmtk-2026/party_prototype/party_script/clothing.gd
@@ -2,11 +2,13 @@ extends Area2D
 
 signal collected
 
+@onready var sprite = $Sprite2D
+
 var dragging = false
 var offset = Vector2.ZERO
 
 
-func _input_event(viewport, event, shape_idx):
+func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
@@ -16,6 +18,9 @@ func _input_event(viewport, event, shape_idx):
 				dragging = false
 
 
-func _process(delta):
+func _process(_delta):
 	if dragging:
+		sprite.frame = 1
 		global_position = get_global_mouse_position() + offset
+	else:
+		sprite.frame = 0
--- gmtk-2026/party_prototype/party_script/laundry_basket.gd
+++ gmtk-2026/party_prototype/party_script/laundry_basket.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
+@onready var animation = $AnimationPlayer
 
 func _on_area_entered(area):
 	print("Clothing collected!")
+	animation.play("clothes_collected")
 	area.collected.emit()
 	area.queue_free()
