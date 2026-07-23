93099802bbc5d6a489e484c798e92d6d06be7189
Rithunyaa <rithunyaa@gmail.com>
2026-07-23 16:10:48 -0400

    Fixed clothing spawing on basket
    bug where clothes kept spawning on top of the laundry basket and immediately getting collected

--- gmtk-2026/party_prototype/party_script/clean_room_level.gd
+++ gmtk-2026/party_prototype/party_script/clean_room_level.gd
@@ -7,46 +7,56 @@ var time_left = 30
 
 @onready var timer_label = $CanvasLayer/TimerLabel
 @onready var counter: Label = $CanvasLayer/ClothesCounter
+@onready var laundry_basket: Area2D = $LaundryBasket
 
 var cleaned_clothes = 0
 var spawn_area = Rect2(100, 100, 950, 450)
 
 
+
 func _ready():
+	randomize()
 	spawn_clothes()
 	update_counter()
 
 
 func spawn_clothes():
+	laundry_basket.monitoring = false
+	
 	var clothes_to_spawn = []
 
 	# Figure out who gets the extra clothing
 	if randi() % 2 == 0:
 		for i in range(3):
 			clothes_to_spawn.append(clothing_type_1)
+
 		for i in range(2):
 			clothes_to_spawn.append(clothing_type_2)
+
 	else:
 		for i in range(2):
 			clothes_to_spawn.append(clothing_type_1)
+
 		for i in range(3):
 			clothes_to_spawn.append(clothing_type_2)
+
 
 	# Randomize their order
 	clothes_to_spawn.shuffle()
 
-	# Spawn them
+
+	# Spawn clothes
 	for clothing_scene in clothes_to_spawn:
 		var clothing = clothing_scene.instantiate()
 
-		var random_position = Vector2(
-			randf_range(spawn_area.position.x, spawn_area.end.x),
-			randf_range(spawn_area.position.y, spawn_area.end.y)
-		)
+		var random_position = get_safe_spawn_position()
 
 		clothing.position = random_position
 		clothing.collected.connect(clothing_collected)
+
 		add_child(clothing)
+
+	laundry_basket.monitoring = true
 
 
 func clothing_collected():
@@ -68,3 +78,17 @@ func _on_party_timer_timeout():
 
 func update_counter():
 	counter.text = "Clothes Cleaned: " + str(cleaned_clothes) + "/" + str(amount_of_clothes)
+	
+func get_safe_spawn_position():
+	var position = Vector2.ZERO
+	
+	while true:
+		position = Vector2(
+			randf_range(spawn_area.position.x, spawn_area.end.x),
+			randf_range(spawn_area.position.y, spawn_area.end.y)
+		)
+		
+		var distance = position.distance_to(laundry_basket.position)
+		
+		if distance > 300:
+			return position
