2b78dcc66c28d94edddc85cf95e74e340925cf14
Rithunyaa <rithunyaa@gmail.com>
2026-07-25 17:35:43 -0400

    BUGS

--- gmtk-2026/party_prototype/party_script/living_room.gd
+++ gmtk-2026/party_prototype/party_script/living_room.gd
@@ -7,13 +7,6 @@ func _ready():
 	update_timer()
 	GameTimer.time_updated.connect(update_timer)
 	
-	var player := $Player
-	
-	match GameManager.previous_scene:
-		"chips_task": player.position = Vector2(805,392)
-	
-	GameManager.previous_scene = "living_room"
-
 
 func update_timer():
 	timer_label.text = "[shake level=6]Party starts in: " + GameTimer.get_time_text()
