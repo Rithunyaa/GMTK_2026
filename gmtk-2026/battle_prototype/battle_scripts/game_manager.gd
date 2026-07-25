af0a29cdb487dab6db255e9256c3c098de418134
Rithunyaa <rithunyaa@gmail.com>
2026-07-25 18:49:23 -0400

    Deleted Battle Prototype
    it was just getting difficult to find things becuase of it

--- gmtk-2026/battle_prototype/battle_scripts/game_manager.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var attack_cards = 0
-var health_potions = 0
-var shields = 0
-
-var previous_scene:String
---
+++ gmtk-2026/party_prototype/party_script/main_menu.gd
@@ -0,0 +1,28 @@
+extends Node
+
+
+var started = false
+
+
+func _input(event):
+
+	if started:
+		return
+
+	# Any keyboard press
+	if event is InputEventKey and event.pressed:
+		start_game()
+
+	# Any mouse click
+	elif event is InputEventMouseButton and event.pressed:
+		start_game()
+
+
+
+func start_game():
+
+	started = true
+
+	get_tree().change_scene_to_file(
+		"res://party_prototype/party_scenes/Bedroom.tscn"
+	)
