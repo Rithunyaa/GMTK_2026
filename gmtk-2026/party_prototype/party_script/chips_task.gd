f552a127dab4113968c1cab60e843909026085ec
Mr-Excoem <chuikeeper@outlook.com>
2026-07-26 14:37:13 +0800

    Let bowl disappear after chips_task done
    and made the task can only done once

--- gmtk-2026/party_prototype/party_script/chips_task.gd
+++ gmtk-2026/party_prototype/party_script/chips_task.gd
@@ -3,14 +3,18 @@ extends Area2D
 var player_near = false
 @onready var instructions: RichTextLabel = $"../CanvasLayer/Instructions"
 
+func _ready():
+	$Sprite2D.visible = not GameTimer.chips_done
 
-func _input_event(viewport, event, shape_idx):
+
+func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			start_laundry_task()
 
 
 func start_laundry_task():
-	await get_parent().get_node("DarkScreen").darker()
-	instructions.visible = false
-	get_tree().change_scene_to_file("res://party_prototype/party_scenes/chips_level.tscn")
+	if not GameTimer.chips_done:
+		await get_parent().get_node("DarkScreen").darker()
+		instructions.visible = false
+		get_tree().change_scene_to_file("res://party_prototype/party_scenes/chips_level.tscn")
